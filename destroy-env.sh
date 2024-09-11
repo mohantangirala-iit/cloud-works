9f55b161e035727f23a09e647a6049b5fe7c3f8d
git_repo_user <evalgit@example.com>
2024-09-11 10:06:18 -0700

    Added destroy scripts

---
+++ destroy-env.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+##############################################################################
+# Module Practice Assessment
+# This assignment requires you to destroy the Cloud assets you created
+# Remember to set you default output to text in the aws config command
+##############################################################################
+
+echo "Beginning destroy script for module-02..."
+
+# Collect Instance IDs
+INSTANCEIDS=aws ec2 describe-instances --filters "Name=instance-type,Values=t2.micro" --query "Reservations[].Instances[].InstanceId"
+
+echo $INSTANCEIDS
+
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/wait/instance-terminated.html
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/terminate-instances.html
+
+if [ "$INSTANCEIDS" != "" ]
+  then
+    aws ec2 terminate-instances --instance-ids $INSTANCEIDS
+    echo "Waiting for all instances report state as TERMINATED..."
+    aws ec2 wait instance-terminated --instance-ids $INSTANCEIDS
+    echo "Finished destroying instances..."
+  else
+    echo 'There are no running values in $INSTANCEIDS to be terminated...'
