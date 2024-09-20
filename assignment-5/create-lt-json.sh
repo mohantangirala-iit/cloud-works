2422a8efae8ae4d9dbfe5b0b3d5d7c3140c32cb1
git_repo_user <evalgit@example.com>
2024-09-20 13:38:51 -0700

    added one more device

--- assignment-5/create-lt-json.sh
+++ assignment-5/create-lt-json.sh
@@ -50,7 +50,7 @@ JSON="{
             \"DeleteOnTermination\": true
         }
     ],
-    \"BlockDeviceMappings\":[{\"DeviceName\":\"/dev/sdc\",\"Ebs\":{\"VolumeSize\": ${18}}}],
+    \"BlockDeviceMappings\":[{\"DeviceName\":\"/dev/sdc\",\"Ebs\":{\"VolumeSize\": ${18}}},{\"DeviceName\":\"/dev/sdb\",\"Ebs\":{\"VolumeSize\": ${18}}}],
     \"ImageId\": \"${1}\",
     \"InstanceType\": \"${2}\",
     \"KeyName\": \"${3}\",
